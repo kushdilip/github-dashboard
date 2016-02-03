b6eb2b9a4bdf165bdbf5ae388e13746b150ed955
Dilip <dilip@artoo.in>
2016-02-04 00:38:50 +0530

    list user organisations

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  url: 'https://api.github.com',
   model(){
-    let accessToken = this.get('session').get('currentUser.accessToken');
-    return null;
+    let url = this.get('url');
+    let username = this.get('session').get('currentUser.username');
+    let orgs = `${url}/users/${username}/orgs`;
+    return Ember.$.ajax({url: orgs});
+  },
+  
+  setupController(){
+    this._super(...arguments);
+  },
+  
+  actions: {
+    getMembers(org){
+      
+    }
   }
 });
