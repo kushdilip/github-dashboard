07ff55584abc826047e0da282e01876e163637ec
Dilip <dilip@artoo.in>
2016-02-09 10:23:17 +0530

    integrate ember-paper and basic templating.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  url: 'https://api.github.com',
+  
   beforeModel: function() {
-    return this.get('session').fetch().then(function() {
+    return this.get('session').fetch().then(function() {    
       console.log('session fetched');
     }, function() {
       console.log('no session to fetch');
