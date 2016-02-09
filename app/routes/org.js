6ec55c11deaa8bf49581924213041d8bd31cf8d1
Dilip <dilip@artoo.in>
2016-02-09 23:52:10 +0530

    integrate power-select and put select bar for selecting repos.

---
+++ app/routes/org.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  url: 'https://api.github.com',
+  accessToken: Ember.computed.alias('session.currentUser.accessToken'),
+  
+  model: function(params) {
+    let org = params.org;
+    let url = this.get('url');
+    let accessToken = this.get('accessToken');
+
+    let repoUrl = `${url}/orgs/${org}/repos?access_token=${accessToken}&per_page=100`;
+    return Ember.$.ajax({url: repoUrl});
+  },
+});
