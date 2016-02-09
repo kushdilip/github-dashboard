12240d603638db745bb304e4ae6c734d48810cb4
Dilip <dilip@artoo.in>
2016-02-09 20:37:14 +0530

    basic overall material layout for home page

--- app/routes/application.js
+++ app/routes/application.js
@@ -11,6 +11,24 @@ export default Ember.Route.extend({
     });
   },
   actions: {
+    signIn: function(provider) {
+      this.controller.set('signingIn',true);
+      this.controller.set('error', null);
+      
+      Ember.run.schedule('afterRender', this, () => {
+        Ember.$('#signin-modal-back').one('click', () => {
+          this.controller.set('signingIn',false);
+        });
+        this.get("session").open("firebase", { provider: provider, settings: {scope:'read:org,repo'}}).then(() => {
+          this.controller.set('signingIn',false);
+          this.transitionTo('home');
+        },
+        (error) => {
+          this.controller.set('signingIn',false);
+          this.controller.set('error', 'Could not sign you in: '+error.message);
+        });  
+      });
+    },
     logout: function() {
       this.get("session").close();
       this.transitionTo('login');
--- app/routes/login.js
+++ app/routes/login.js
@@ -5,26 +5,5 @@ export default Ember.Route.extend({
     if (this.get('session').get('isAuthenticated')) {
       this.transitionTo('index');
     }
-  }.on('activate'),
-  
-  actions: {
-    signIn: function(provider) {
-      this.controller.set('signingIn',true);
-      this.controller.set('error', null);
-      
-      Ember.run.schedule('afterRender', this, () => {
-        Ember.$('#signin-modal-back').one('click', () => {
-          this.controller.set('signingIn',false);
-        });
-        this.get("session").open("firebase", { provider: provider, settings: {scope:'read:org,repo'}}).then(() => {
-          this.controller.set('signingIn',false);
-          this.transitionTo('home');
-        },
-        (error) => {
-          this.controller.set('signingIn',false);
-          this.controller.set('error', 'Could not sign you in: '+error.message);
-        });  
-      });
-    }
-  }
+  }.on('activate')
 });
