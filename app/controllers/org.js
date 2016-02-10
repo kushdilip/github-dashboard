d0fb6a81631dcf02077ac7d3030e14c2af61ce32
Dilip <dilip@artoo.in>
2016-02-10 23:14:40 +0530

    moved action from route to controller

--- app/controllers/org.js
+++ app/controllers/org.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  
+  actions: {
+    getRepoStat(){
+      let repos = this.get('repo');
+      if (!Ember.isArray(repos)) {
+        return;
+      }
+      
+      let url = this.get('url');
+      let accessToken = this.get('accessToken');
+      
+      let urls = repos.map(repo => `${url}/repos/${repo.full_name}/stats/contributors?access_token=${accessToken}`);
+      let fns = urls.map(url => Ember.$.ajax({url: url}));
+      
+      Ember.RSVP.all(fns).then(contributions => this.set('contributions', contributions));
+    }  
+  }
 });
